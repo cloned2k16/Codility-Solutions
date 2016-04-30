ffc287cb0f5c0aac74abea71e16078b35bc11e69
i5 <know@ledg.es>
2016-04-30 12:56:13 +0200

    BinaryGap solution

---
+++ src/codility/BinaryGap/Solution.java
@@ -0,0 +1,31 @@
+package codility.BinaryGap;
+
+	// you can also use imports, for example:
+	// import java.util.*;
+
+	// you can write to stdout for debugging purposes, e.g.
+	// System.out.println("this is a debug message");
+
+	class Solution {
+	    public int solution(int N) {
+	        int mask    =   0x80000000
+	        ,   gap     =   0
+	        ,   currGap =  -1
+	        ;
+	        
+	        for (int i = 0; i < 32; i++) {                  // loop through all bits because it is safer
+	                                                        // than a while loop ..
+	            int hit = N & mask;
+	            if (0 == hit) {                             
+	                if (currGap>=0) currGap++;              // 0 and already hit 1
+	            }    
+	            else {
+	              gap = (gap >= currGap) ? gap : currGap;   // if current gap > gap use it
+	              currGap = 0;                              // reset currGap
+	            }    
+	            if (0 == (N & ~mask)) break;               // we are done!
+	            mask>>>=1;
+	        }    
+	        return gap;
+	    }
+	}
