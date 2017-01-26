26877f615664bd40e2e361f9842ebc4a4185e475
i5 <know@ledg.es>
2017-01-26 12:05:27 +0100

    tabs 2 spaces

--- src/codility/PermCheck/Solution.java
+++ src/codility/PermCheck/Solution.java
@@ -1,28 +1,28 @@
 package codility.PermCheck;
 
- 
+
 import net.yaoo.CodilityPlayaR;
 
-public class 					Solution {
-	   class                    Logger
-	   extends 					CodilityPlayaR						{   
-	   }
-	   
-	   public int 				solution(int[] A)					{
-		    if (null == A)                          				return 0;
-	        int     len     =   A.length
-	        ,	 	bmp []  =   new int [len]
-      		;
-		        
-	        for (int i=0; i < len ; i++) {
-	            int v = A[i] -1;                                // zero-indexed
-	            if (0 > v || v >= len)              return 0;   // falling outside range
-	            if (0!=bmp[v])                      return 0;   // duplicated value
-	            bmp[v] = 1;                                     // first hit
-	        }    
-	        
-	        return 1;
-	   }
-	  
+public class                    Solution {
+       class                    Logger
+       extends                  CodilityPlayaR                      {
+       }
+
+       public int               solution(int[] A)                   {
+            if (null == A)                                          return 0;
+            int     len     =   A.length
+            ,       bmp []  =   new int [len]
+            ;
+
+            for (int i=0; i < len ; i++) {
+                int v = A[i] -1;                                // zero-indexed
+                if (0 > v || v >= len)              return 0;   // falling outside range
+                if (0!=bmp[v])                      return 0;   // duplicated value
+                bmp[v] = 1;                                     // first hit
+            }
+
+            return 1;
+       }
+
 
 }
