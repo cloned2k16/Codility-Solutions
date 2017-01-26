e0d701bddc9c4fe99f776824a54d8c3bab060b15
i5 <know@ledg.es>
2017-01-26 12:04:42 +0100

    tabs 2 spaces

--- src/codility/MaxCounters/Solution.java
+++ src/codility/MaxCounters/Solution.java
@@ -1,28 +1,28 @@
-package codility.MaxCounters; 
+package codility.MaxCounters;
 
-public class 					Solution 							{
+public class                    Solution                            {
 
-	public int[] 				solution							(int N, int[] A) 				{
-		int res [] 	= new int [N]
-		,	max		= 0
-		,	len		= A.length
-		;
-		
-		for ( int i=0; i <len ; i++) {
-			int idx = A[i];
-			if (1<= idx && idx <= N) {
-				int v = res[idx-1] +1;
-				max = v > max ? v : max;
-				res[idx-1]=v;
-			}
-			else if ( idx == N+1) {
-				for (int k=0 ; k < N; k++) res[k] = max;
-			}
-			else { // WTF ?
-				
-			}
-		}
-		
-		return res;
-	}
+    public int[]                solution                            (int N, int[] A)                {
+        int res []  = new int [N]
+        ,   max     = 0
+        ,   len     = A.length
+        ;
+
+        for ( int i=0; i <len ; i++) {
+            int idx = A[i];
+            if (1<= idx && idx <= N) {
+                int v = res[idx-1] +1;
+                max = v > max ? v : max;
+                res[idx-1]=v;
+            }
+            else if ( idx == N+1) {
+                for (int k=0 ; k < N; k++) res[k] = max;
+            }
+            else { // WTF ?
+
+            }
+        }
+
+        return res;
+    }
 }
